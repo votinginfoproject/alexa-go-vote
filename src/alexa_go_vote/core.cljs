8c3dff9ea3e2ecd83c4bd3c6da0aebc1131ee76f
Troy Hill <troy@democracy.works>
2018-10-15 10:20:35 -0600

    Adjust launch response to check for device address

--- src/alexa_go_vote/core.cljs
+++ src/alexa_go_vote/core.cljs
@@ -1,12 +1,37 @@
 (ns alexa-go-vote.core
   (:require [cljs-lambda.macros :refer-macros [deflambda]]
+            [alexa-go-vote.address-api :as aa]
             [alexa-go-vote.logging :as log]
             [alexa-go-vote.polling-place :as pp]))
 
-(def launch-response
+(def standard-launch-response
   {:version 1.0
-   :response {:outputSpeech {:type "PlainText" :text "Welcome to Go Vote! I can help you find your polling place. Just ask me 'Where do I vote'?"}
-              :shouldEndSession false}})
+   :response
+   {:outputSpeech
+    {:type "PlainText"
+     :text "Welcome to Go Vote! I can help you find your polling place. Just ask me 'Where do I vote'?"}
+    :shouldEndSession false}})
+
+(defn launch-response
+  "Attempts to get the address from the Device Address API, can succeed if this
+  is a returning user. If we get back an address, store it in the session to be
+  used once the user activates the polling place intent."
+  [context]
+  (aa/retrieve-address
+   context
+   (fn [address-response]
+     (let [resp standard-launch-response]
+       (if (string? address-response)
+         (merge resp {:sessionAttributes {:full_address address-response}})
+         (if (= :unauthorized address-response)
+           (merge resp {:sessionAttributes
+                        {:addressApiLookupCount 1}
+
+                        :card
+                        {:type "AskForPermissionsConsent"
+                         :permissions ["read::alexa:device:all:address"]}})
+           (merge resp {:sessionAttributes
+                        {:addressApiLookupCount 1}})))))))
 
 (defn fallback-response [evt]
   (log/error "default response enacted for event:" (pr-str evt))
@@ -36,15 +61,14 @@
    the input map to call functions to build the proper response map"
   [event ctx]
   (log/debug "Incoming Event:" (pr-str event))
-  (let [{:keys [session request
-                version timestamp]} event
+  (let [{:keys [context request]} event
         type (:type request)
         intent (get-in request [:intent :name])]
     ;; If we ever offer more of our own intents than just pollingPlace
     ;; we should restructure this a bit more cleanly with a multimethod
    (cond
-     (= type "LaunchRequest") launch-response
      (= intent "pollingPlace") (pp/intent request)
+     (= type "LaunchRequest") (launch-response context)
      (= intent "AMAZON.HelpIntent") help-response
      (= intent "AMAZON.StopIntent") stop-response
      (= intent "AMAZON.CancelIntent") stop-response
